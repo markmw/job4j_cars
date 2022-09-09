983298bb1faea16b4138adf7e48ac7b0b2959aca
markmw <dant1kkz@gmail.com>
2022-09-09 16:11:44 +0600

    1. Техническое задание - проект "АвтоМаг" [#504895 #341431]

---
+++ db/scripts/001_ddl_init_schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE auto_user (
+                       id SERIAL PRIMARY KEY,
+                       login VARCHAR NOT NULL UNIQUE,
+                       password VARCHAR NOT NULL
+);
+
+CREATE TABLE auto_post (
+                     id SERIAL PRIMARY KEY,
+                     text VARCHAR NOT NULL,
+                     created TIMESTAMP NOT NULL,
+                     auto_user_id INTEGER REFERENCES auto_user
+);
