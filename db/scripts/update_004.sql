a30c416106aa2eca42bd208a6795a0d220e45754
markmw <dant1kkz@gmail.com>
2022-09-26 10:35:39 +0600

    1. ToMany [#301848 #346538]

--- db/scripts/update_004.sql
+++ db/scripts/update_004.sql
@@ -2,7 +2,7 @@ CREATE TABLE PRICE_HISTORY(
    id SERIAL PRIMARY KEY,
    before BIGINT not null,
    after BIGINT not null,
-   created TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+   created TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
 );
 
 ALTER TABLE auto_post ADD COLUMN price_history_id INT REFERENCES price_history(id);
