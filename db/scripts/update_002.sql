e759c0130ed633b0113295f5d6228386d2fd75f4
markmw <dant1kkz@gmail.com>
2022-10-04 17:07:46 +0600

    Модели и связи. Машины и владельцы [#4744 #348592] - исправление в sql-скриптах

--- db/scripts/update_002.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO auto_user(login, password) VALUES ('Goncharenko', 'root');
-INSERT INTO auto_user(login, password) VALUES ('Kononenko', 'root');
-INSERT INTO auto_user(login, password) VALUES ('Vlasov', 'root');
