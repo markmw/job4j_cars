a2b035134eeace89ff9b22666773ccb580437bf0
markmw <dant1kkz@gmail.com>
2022-10-07 12:49:14 +0600

    2. Интеграционные тесты для проект job4j_cars [#4746]

--- src/test/java/ru/job4j/cars/repository/PostRepositoryTest.java
+++ src/test/java/ru/job4j/cars/repository/PostRepositoryTest.java
@@ -9,61 +9,54 @@ import org.junit.jupiter.api.Test;
 import ru.job4j.cars.model.*;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
 import java.util.List;
 import java.util.function.Function;
 
 class PostRepositoryTest {
     private final CrudRepository crudRepository = new CrudRepository(
             new MetadataSources(new StandardServiceRegistryBuilder()
-                    .configure().build()).buildMetadata().buildSessionFactory());
+                    .configure().build())
+                    .buildMetadata().buildSessionFactory());
     private final PostRepository postRepository = new PostRepository(crudRepository);
 
     @BeforeEach
     public void clearDataBase() {
-        crudRepository.tx((Function<Session, Object>)
-                session -> session.createQuery("delete from Post").executeUpdate());
+        crudRepository.tx((Function<Session, Object>) session -> session.createQuery(
+                "delete from Post").executeUpdate());
     }
 
     @Test
-    public void whenFindPostToLastDay() {
-        Post oldPost = new Post(
-                1,
-                "text",
-                LocalDateTime.of(2000, 1, 10, 14, 15),
-                new User(),
-                new HashSet<>()
-        );
-        Post newPost = new Post(
-                2,
-                "text2",
-                LocalDateTime.of(2022, 10, 6, 10, 00),
-                new User(),
-                new HashSet<>()
-        );
-        postRepository.add(oldPost);
-        postRepository.add(newPost);
-        Assertions.assertThat(postRepository.findPostToLastDay()).isEqualTo(newPost);
-    }
-
-    @Test
-    public void whenFindPostWithPhoto() {
+    public void whenSavePost() {
         Post post = new Post();
         post.setText("post_text");
-        postRepository.add(post);
-        Post savedPost = (Post) postRepository.findPostWithPhoto();
+        postRepository.save(post);
+        Post savedPost = postRepository.getById(post.getId()).orElse(new Post());
         Assertions.assertThat(savedPost.getText()).isEqualTo(post.getText());
     }
 
     @Test
-    public void whenFindPostWithModel() {
+    public void whenGetPostWithTextLikeKey() {
         Post firstPost = new Post();
         Post secondPost = new Post();
         String key = "key";
         firstPost.setText("key_of_post");
         secondPost.setText("post_of_key");
-        postRepository.add(firstPost);
-        postRepository.add(secondPost);
-        Assertions.assertThat(postRepository.findPostWithModel(key)).isEqualTo(List.of(firstPost, secondPost));
+        postRepository.save(firstPost);
+        postRepository.save(secondPost);
+        Assertions.assertThat(postRepository.like(key)).isEqualTo(List.of(firstPost, secondPost));
+    }
+
+    @Test
+    public void whenGetLastDayPost() {
+        Post oldPost = new Post();
+        Post newPost = new Post();
+        LocalDateTime oldDateTime = LocalDateTime.of(2000, 1, 10, 14, 15);
+        LocalDateTime newDateTime = LocalDateTime.of(2010, 1, 10, 14, 15);
+        oldPost.setCreated(oldDateTime);
+        newPost.setCreated(newDateTime);
+        postRepository.save(oldPost);
+        postRepository.save(newPost);
+        Post post = postRepository.getLastDay().get(0);
+        Assertions.assertThat(postRepository.getLastDay().get(0)).isEqualTo(newPost);
     }
 }
